01dc2fe911804bcf8a31b75eb7808332f16954ed
Gauransh Sirohi <gauranshsirohi@gmail.com>
2024-11-15 19:20:42 +0530

    Initial commit

---
+++ src/components/web/customer/desclaimer/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+export default class Desclaimer extends Component {
+    render() {
+        return (
+            <div>
+                <div className="banner-top">
+                    <div className="container">
+                        <h3>Desclaimer</h3>
+                        <div className="clearfix"> </div>
+                    </div>
+                </div>
+                <div className="terms">
+                    <div className="container">
+                        <div className="spec ">
+                            <h3>Desclaimer</h3>
+                            <div className="ser-t">
+                                <b />
+                                <span><i /></span>
+                                <b className="line" />
+                            </div>
+                        </div>
+                        <div className="ter-wthree">
+                            <h6><span>1.</span>Important Points</h6>
+                            <ol start={1}>
+                                <li>Welcome to Big store. By using our website, you agree to the following terms and conditions. Please read them carefully:</li>
+
+<li>Product Information: We strive to provide accurate and up-to-date information about our products, but we do not guarantee the complete accuracy, reliability, or availability of product descriptions, prices, and images. All products are subject to availability and may vary in appearance from those shown on the website.</li>
+
+<li>Pricing: Prices on the website are subject to change without notice. In the event of an incorrect price due to an error, we reserve the right to cancel or refuse orders at the incorrect price.</li>
+
+<li>Order Fulfillment: Orders are subject to stock availability. We will notify you if any item is out of stock and offer alternatives or cancellations as appropriate.</li>
+
+<li>Food Safety: While we take great care in handling and packaging food products, it is the responsibility of the customer to ensure proper storage and preparation to maintain food safety standards.</li>
+
+<li>Refund and Returns: Due to the perishable nature of food products, we do not accept returns or refunds unless the product is defective or damaged upon arrival. Please contact us immediately if this occurs.</li>
+                            </ol>
+                        </div>
+                       
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
